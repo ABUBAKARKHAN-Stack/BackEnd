ac0fa3efe8819526cdd5a64c57de0915abbb3ec2
Abubakar-Aijaz <abubakar123456654123@gmail.com>
2024-09-26 01:21:02 +0500

    Mongo DB Connection

---
+++ vidTube/src/app.js
@@ -0,0 +1,38 @@
+import express, { json } from 'express'
+import cors from 'cors'
+import logger from './logger.js'
+import morgan from 'morgan';
+const app = express()
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    })
+)
+
+// Common Middleware
+app.use(json({ limit: '16kb' }))
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))
+app.use(express.static('public'))
+
+//    ++++++++++++++++++++++++++++++++++ Advance Loggers Morgon Winston ++++++++++++++++++++++++++++++++++++++++++++++++
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+    morgan(morganFormat, {
+        stream: {
+            write: (message) => {
+                const logObject = {
+                    method: message.split(" ")[0],
+                    url: message.split(" ")[1],
+                    status: message.split(" ")[2],
+                    responseTime: message.split(" ")[3],
+                };
+                logger.info(JSON.stringify(logObject));
+            },
+        },
+    })
+);
+
+
+export default app
--- vidTube/src/index.js
+++ vidTube/src/index.js
@@ -1,29 +1,17 @@
-import express from "express";
-import logger from "./logger.js";
-import morgan from "morgan";
-const app = express()
+import dotenv from 'dotenv'
+import app from "./app.js";
+import logger from './logger.js';
+import connectDB from './db/index.js';
 
-
-
-const morganFormat = ":method :url :status :response-time ms";
-
-app.use(
-    morgan(morganFormat, {
-        stream: {
-            write: (message) => {
-                const logObject = {
-                    method: message.split(" ")[0],
-                    url: message.split(" ")[1],
-                    status: message.split(" ")[2],
-                    responseTime: message.split(" ")[3],
-                };
-                logger.info(JSON.stringify(logObject));
-            },
-        },
-    })
-);
-
+dotenv.config({
+    path: "./.env"
+})
 
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`APP is running on PORT ${PORT}`))
+
+connectDB()
+.then(()=>{
+    app.listen(PORT, () => logger.info(`APP is running on PORT ${PORT}`))
+})
+.catch((err) => logger.error('Mongo DB Connection Error' , err))
